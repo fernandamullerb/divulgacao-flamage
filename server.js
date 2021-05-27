f4c2d3fd8d2e9dfed349017876d382b3c962ae2d
Fernanda Müller Baldi <fernandamullerb@gmail.com>
2021-05-27 20:10:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
-app.listen(3000);
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(bodyParser.urlencoded());
-
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
@@ -18,13 +16,25 @@ app.get('/about', (req, res) => {
     res.render('pages/about');
 });
 
+app.get('/contact', (req, res) => {
+    res.render('pages/contact')
+})
+
 app.get('/prices', (req, res) => {
     res.render('pages/prices');
 });
 
-app.get('/contact', (req, res) => {
-    res.render('pages/contact')
-  })
+app.post('/prices', (req, res) => {
+
+    var feature = req.body.feature 
+
+        if (feature = true) {
+            const totalPrice = Math.ceil(totalPrice + featurePrice)
+        }
+
+        res.render('pages/prices', totalPrice);
+});
+
 
 app.listen(3333);
 console.log('3333 is the magic port');
